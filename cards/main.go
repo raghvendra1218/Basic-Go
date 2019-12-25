118c846458b240a25af8b897a373f7c4a828dbe6
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-12-24 23:45:09 -0800

    Add design deck of cards game

---
+++ cards/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Oh look it's joker"
+	card := "Ace of spades" //Declare and assign
+	card = "Five of club"   //Reassigning the value to a variable
+	fmt.Println(card)
+}
