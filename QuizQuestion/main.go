2c7032361fdbb1cb16e2d9dd1aa7be1850fd2b04
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-12-25 00:18:11 -0800

    Add quiz Question

--- QuizQuestion/main.go
+++ QuizQuestion/main.go
@@ -1,3 +1,27 @@
+/**
+Here's a tough one!  Imagine we have two files in the same folder with the same package name.  Will the following code successfully compile?  This might take a little experimentation on your side.  If you do try this out yourself, run your code with the command go run main.go state.go .
+
+In main.go:
+
+package main
+
+func main() {
+    printState()
+}
+
+
+In a separate file called state.go:
+
+package main
+
+import "fmt"
+
+func printState() {
+    fmt.Println("California")
+}
+Will this compile and run successfully ?
+**/
+
 package main
 
 func main() {
