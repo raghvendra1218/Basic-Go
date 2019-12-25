df04f79389784c612c5393eaa131fd98a7530fcb
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-12-25 11:54:43 -0800

    add custom type object

--- cards/main.go
+++ cards/main.go
@@ -1,18 +1,15 @@
 package main
 
-import "fmt"
-
 func newCard() string {
 	return "Five of Spades"
 }
 
 func main() {
-	cards := []string{"Ace of heart", newCard()}
+	cards := deck{"Ace of heart", newCard()}
 	// append function does not modify existing slice,
 	//instead it returns a new slice and assigns back to cards slice
 	cards = append(cards, "Joker!")
-	//Let's iterate over slice
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	//calling print function of custom type object
+	deck.print(cards)
+
 }
